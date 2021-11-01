3f492ab52dd8121befa8144c6b60ecc3b6017e97
warren yun <xxwarrenhdxx@gmail.com>
2021-11-01 03:23:54 -0400

    Implement getting request body in get symptom

--- api/main.go
+++ api/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	
 	"github.com/NebuDev14/Diseasy-Peasy/lib"
+	"github.com/gin-gonic/gin"
 )
+
+type SymptomQuery struct {
+	Symptoms	[]string
+}
 
 func getDiseaseByPart(c *gin.Context) {
 	part := c.Param("part")
@@ -13,21 +20,23 @@ func getDiseaseByPart(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, disease)
 }
 
-
 func getDiseaseByName(c *gin.Context) {
 	name := c.Param("name")
 	disease := lib.FindDiseaseByName(name)
 	c.IndentedJSON(http.StatusOK, disease)
 }
 
-// func getSymptom(c *gin.Context) {
-// 	name := c.Param("name")
-// 	symptom := lib.FindSymptom(name)
-// 	c.IndentedJSON(http.StatusOK, symptom)
-// }
+func getSymptom(c *gin.Context) {
+	jsonData, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil { fmt.Println(err) }
+	var data SymptomQuery
+	json.Unmarshal(jsonData, &data)
+	fmt.Println(data.Symptoms[0])
+	
+	c.IndentedJSON(http.StatusOK, "no")
+}
 
 func main() {
-	
 	fmt.Println(lib.FindDiseaseByName("test2"))
 	symps := []string{"memory loss", "pain"};
 	lib.CreateDisease("Alzheimers", "Brain", symps)
